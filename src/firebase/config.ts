ca00582d8a6406666785507c49c7697d15149a55
Samanea Kuliner Junction Indonesia <samaneakulinerjunction.id@gmail.com>
2025-10-29 07:21:08 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5342684498-154ad",
+  "appId": "1:835505489767:web:78d2c34c3dc241e9b251c2",
+  "apiKey": "AIzaSyAWGNqmYqoxIDv-AO2V7cor2nMXQ7w68t8",
+  "authDomain": "studio-5342684498-154ad.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "835505489767"
+};
